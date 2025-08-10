71ee6491247e048732f6775efb395e2aee77f693
Pedram Hatef <Mr.pd33@yahoo.com>
2025-08-10 08:08:02 +0000

    I don't have this project name (bybit-balance-view) on my console. maybe

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,6 +2,10 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// This is your Firebase configuration.
+// The `projectId` is the unique ID for your project.
+// In the Firebase Console (https://console.firebase.google.com/), you can find
+// this project by looking for the project ID "bybit-balance-view" under the project name.
 const firebaseConfig = {
   "projectId": "bybit-balance-view",
   "appId": "1:701023944234:web:fc234862537d441eae0d53",
