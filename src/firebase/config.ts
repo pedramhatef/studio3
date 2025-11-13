f178996573511f4700eafa35b6b27c39d998e246
Pedram Hatef <Mr.pd33@yahoo.com>
2025-11-13 01:42:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "bybit-balance-view",
+  "appId": "1:701023944234:web:fc234862537d441eae0d53",
+  "apiKey": "AIzaSyBTKk45FMy6sGdeErxwTfx8gP_Upz1oqKY",
+  "authDomain": "bybit-balance-view.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "701023944234"
+};
